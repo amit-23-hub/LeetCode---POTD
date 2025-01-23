b1fef66e2f95589b338616a3a1e11432ec886e63
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2025-01-23 10:17:12 +0530

    Time: 3 ms (66.90%) | Memory: 8.3 MB (9.63%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
- /// using Dp  
+//  /// using Dp  memoizationn
 
 
-      int fibon(int n , vector<int>&dp){
-        if(n<=1){
-            return n ;  
-        }
-        // agr already value calculated hai to we directly use them 
-        if(dp[n]!=-1){
-            return dp[n] ; 
-        }
-        // jo value caclulate krenge use store kr lenge 
-        return dp[n] = fibon(n-1 , dp)+fibon(n-2 , dp) ;
-      }
+//       int fibon(int n , vector<int>&dp){
+//         if(n<=1){
+//             return n ;  
+//         }
+//         // agr already value calculated hai to we directly use them 
+//         if(dp[n]!=-1){
+//             return dp[n] ; 
+//         }
+//         // jo value caclulate krenge use store kr lenge 
+//         return dp[n] = fibon(n-1 , dp)+fibon(n-2 , dp) ;
+//       }
 
-    int fib(int n) {
-        vector<int>dp(n+1 , -1) ; 
-        return fibon(n , dp ) ; 
+//     int fib(int n) {
+//         vector<int>dp(n+1 , -1) ; 
+//         return fibon(n , dp ) ; 
+//     }
+
+int fib(int n){
+     if(n<=1){
+        return n ; 
+     }
+    vector<int>dp(n+1) ; 
+    dp[0]=0 ; 
+    dp[1] = 1 ; 
+
+    for(int i =2 ; i<n+1 ; i++){
+        dp[i] = dp[i-1]+dp[i-2] ; 
     }
+    return dp[n] ; 
+}
 };
