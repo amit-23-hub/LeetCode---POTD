902efa31d28185a1819b709a8dbc354df8875b5e
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2025-01-23 10:02:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (16.84%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
-    int fib(int n) {
-        // base case 
-        if(n<=1) {
-            return n ;
+ /// using Dp  
+
+
+      int fibon(int n , vector<int>&dp){
+        if(n<=1){
+            return n ;  
         }
+        // agr already value calculated hai to we directly use them 
+        if(dp[n]!=-1){
+            return dp[n] ; 
+        }
+        // jo value caclulate krenge use store kr lenge 
+        return dp[n] = fibon(n-1 , dp)+fibon(n-2 , dp) ;
+      }
 
-
-        return fib(n-1) + fib(n-2) ; 
+    int fib(int n) {
+        vector<int>dp(n+1 , -1) ; 
+        return fibon(n , dp ) ; 
     }
 };
