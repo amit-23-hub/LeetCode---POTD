031a6535bb5cb5c7670ddfe4f9cd14e9b9988173
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2025-01-23 10:23:01 +0530

    Time: 3 ms (66.90%) | Memory: 7.8 MB (33.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -20,17 +20,36 @@ public:
 //         return fibon(n , dp ) ; 
 //     }
 
+
+// tabulation method 
+
+// int fib(int n){
+//      if(n<=1){
+//         return n ; 
+//      }
+//     vector<int>dp(n+1) ; 
+//     dp[0]=0 ; 
+//     dp[1] = 1 ; 
+
+//     for(int i =2 ; i<n+1 ; i++){
+//         dp[i] = dp[i-1]+dp[i-2] ; 
+//     }
+//     return dp[n] ; 
+// }
+
+
+// space optimazation 
 int fib(int n){
      if(n<=1){
         return n ; 
      }
-    vector<int>dp(n+1) ; 
-    dp[0]=0 ; 
-    dp[1] = 1 ; 
+    int cur , pre1=1 , pre2 = 0 ; 
 
     for(int i =2 ; i<n+1 ; i++){
-        dp[i] = dp[i-1]+dp[i-2] ; 
+     cur = pre1+pre2 ;
+     pre2=pre1 ; 
+     pre1 = cur ; 
     }
-    return dp[n] ; 
+    return cur ; 
 }
 };
