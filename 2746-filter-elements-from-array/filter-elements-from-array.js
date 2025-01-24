95b4069307b95404f28ad361f9f85f5c69947bad
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2025-01-24 09:32:11 +0530

    Time: 50 ms (75.04%) | Memory: 49.3 MB (20.74%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let fill = [] ; 
+    for(let i = 0 ; i<arr.length ; i++){
+        if(fn(arr[i],i)){
+            fill.push(arr[i]);
+        }
+    }
+    return fill ; 
+};
