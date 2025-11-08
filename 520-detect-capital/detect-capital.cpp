497dd9ae2dedd2ab3d64acefa1b6b24c939701cf
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2025-11-08 16:00:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (19.41%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.size() ; 
+          int c = 0 ; 
+        for(int i = 0 ; i<n;i++){
+            if(isupper(word[i])){
+                 c++ ; 
+                
+            }
+        }
+        if(c==n || c==0 || c==1 && isupper( word[0])){
+          return  true ;
+        }
+     return false  ; 
+    }
+};
